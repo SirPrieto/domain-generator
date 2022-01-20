da496a9d458a16dd39f394cada2159275eb2453f
SirPrieto <hi@abrahamprieto.com>
2022-01-20 03:34:01 +0000

    main

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domains = [".com", ".net"];
+  var domainNames = [];
+  for (var pindex = 0; pindex < pronoun.length; pindex++) {
+    for (var aindex = 0; aindex < adj.length; aindex++) {
+      for (var nindex = 0; nindex < noun.length; nindex++) {
+        for (var dindex = 0; dindex < domains.length; dindex++) {
+          domainNames.push(
+            pronoun[pindex] + adj[aindex] + noun[nindex] + domains[dindex]
+          );
+        }
+      }
+    }
+  }
+  console.log(domainNames);
+
+  let list = document.querySelector(".myList");
+
+  domainNames.forEach(item => {
+    let li = document.createElement("li");
+    li.innerText = item;
+    list.appendChild(li);
+  });
 };
